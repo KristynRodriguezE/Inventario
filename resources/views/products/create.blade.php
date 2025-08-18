1ac6af64c3d93c3e16882a832723b9635bac4172
Mim-xml <kristyn.rodriguez@tecnacional.edu.ni>
2025-08-18 10:46:46 -0600

    feat: Vistas y Rutas

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,3 +1,29 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
+@extends('layouts.panel')
+@section('title', 'Productos/Crear')
+@section('content')
+
+<div class="col-x1-12 order-x1-1">
+    <div class="card bg-secondary shadow">
+        <div class="card-header bg-white border-0">
+            <div class="row align-items-center">
+                <div class="col-8">
+                    <h3 class="mb-0">  <i class="fas fa-plus-circle"> </i> Registrar Producto</h3>
+                </div>
+                <div class="col-4 text-right">
+                    <a href="{{ route('products.index') }}" class="btn btn-sm btn-primary">
+                        <i class="fas fa-arrow-left"></i> Volver
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body container-fluid">
+            <form action="{{ route("products.store") }}" method="POST">
+                @csrf
+                @include('products.form')
+            </form>
+        </div>
+    </div>
+
 </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,42 @@
 <?php
 
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\InventoryController;
+use App\Http\Controllers\ProductEntryController;
+use App\Http\Controllers\ProductOutputController;
+use App\Http\Controllers\MarkController;
+use App\Http\Controllers\CategoryController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+
+    Route::prefix('/profile')->group(function () {
+        Route::get('/', [ProfileController::class, 'index'])->name('profile.index');
+        Route::get('/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/update', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/destroy', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::resource('/products', ProductController::class);
+    Route::resource('/suppliers', SupplierController::class);
+    Route::resource('/intories', InventoryController::class);
+    Route::resource('/productentries', ProductEntryController::class);
+    Route::resource('/productoutput',  ProductOutputController::class);
+    Route::resource('/marks', MarkController::class);
+    Route::resource('/categories', CategoryController::class);
+
+});
+
+require __DIR__ . '/auth.php';
