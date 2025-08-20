36658c589bc34b5e9d68fd6f40b63782df1e49f0
Mim-xml <kristyn.rodriguez@tecnacional.edu.ni>
2025-08-20 07:51:40 -0600

    feat: agregar vistas

--- resources/views/categories/form.blade.php
+++ resources/views/categories/form.blade.php
@@ -11,6 +11,17 @@
     </div>
 </div>
 
+<div class="row">
+    <div class="col-lg-6">
+        <div class="form-group">
+            <label class="form-control-label" for="mark">Nombre de la Categoria</label>
+            <input type="text" id="mark" name="mark" class="form-control form-control-alternative"
+                placeholder="Ingresar el nombre de la marca"
+                value="{{ old('mark', $marks->mark) }}">
+        </div>
+    </div>
+</div>
+
 <hr class="my-4">
 
 <h6 class="heading-small text-muted mb-4">Guardar</h6>
