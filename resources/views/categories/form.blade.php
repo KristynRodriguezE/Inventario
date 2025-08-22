4fcb895efb4690bca3b3b6b5dae011cf2f12097a
Mim-xml <kristyn.rodriguez@tecnacional.edu.ni>
2025-08-22 07:39:48 -0600

    feat: actualizaciones

--- resources/views/categories/form.blade.php
+++ resources/views/categories/form.blade.php
@@ -14,7 +14,7 @@
 <div class="row">
     <div class="col-lg-6">
         <div class="form-group">
-            <label class="form-control-label" for="mark">Nombre de la Categoria</label>
+            <label class="form-control-label" for="mark">numero de la Categoria</label>
             <input type="text" id="mark" name="mark" class="form-control form-control-alternative"
                 placeholder="Ingresar el nombre de la marca"
                 value="{{ old('mark', $marks->mark) }}">
